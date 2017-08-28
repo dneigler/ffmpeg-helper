68d3285d89b47e0dbac8ca8bb589a74ab1d624a3
David Neigler <dneigler@neigler-family-imac.local>
2017-08-27 20:58:39 -0400

    Added logging and mongodb upsert.

--- ParseWmaFiles.py
+++ ParseWmaFiles.py
@@ -6,12 +6,20 @@
 
 import os, sys, subprocess, shlex, re, json
 import fnmatch
+import logging
 from subprocess import call
 import pymongo
 from pymongo import MongoClient
+
 client = MongoClient()
 db = client.music_mongodb
 music_records = db.music_records
+logging.basicConfig(filename='parse_wma_files.log', level=logging.DEBUG)
+logger = logging.getLogger()
+
+music_path = "/Volumes/DATA/ServerFolders/Music"  # /Abbey Lincoln"  # A\ Turtle\'s\ Dream
+music_records.create_index([('format.filename', pymongo.ASCENDING)], unique=True)
+
 
 def probe_file(filename):
     # os.chdir(os.path.expanduser("~/ffmpeg"))
@@ -21,24 +29,25 @@ def probe_file(filename):
     cmnd = [ffprobe_path, '-v', 'quiet', '-print_format', 'json', '-show_format', '-hide_banner', filename]
     # p = subprocess.Popen(cmnd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     p = json.loads(subprocess.check_output(cmnd, stderr=subprocess.STDOUT))
-    print(p)
-    result = music_records.insert_one(p)
-    print('One post: {0}'.format(result.inserted_id))
 
-    # print(filename)
-    # out, err =  p.communicate()
-    # print("==========output==========")
-    # print(out)
-    # if err:
-    #     print("========= error ========")
-    #     print(err)
+    record_filename = p['format']['filename']
+    logger.debug('record-filename: ' + record_filename)
+    logger.debug(p)
+    # {'format': {'filename': "/Volumes/DATA/ServerFolders/Music/Abbey Lincoln/A Turtle's Dream/01_Throw It Away.wma"
+    query = {'format.filename': record_filename}
+    logger.debug('mongo findOne: ')
+    logger.debug(music_records.find_one(query))
+    result = music_records.replace_one(query, p, True)
+    logger.debug('One post: {0}'.format(result))
 
-music_path = "/Volumes/DATA/ServerFolders/Music/Abbey Lincoln" # A\ Turtle\'s\ Dream
 
+logger.info('Starting the Run')
 for root, dirs, files in os.walk(music_path):
     path = root.split(os.sep)
-    print((len(path) - 1) * '---', os.path.basename(root))
+    logger.debug((len(path) - 1) * '--- ' + os.path.basename(root))
     for file in files:
-        print(len(path) * '---', file, os.path.join(root, file))
+        logger.debug(len(path) * '--- ' + file + ' ' + os.path.join(root, file))
         if file.endswith(".wma"):
             probe_file(os.path.join(root, file))
+
+logger.info('Completed Run')
