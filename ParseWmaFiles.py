a7eda4402e3c2a0b7d1dc16218f1cfceb10f3a2f
David Neigler <dneigler@gmail.com>
2017-09-09 23:01:04 -0400

    Removed comments.

--- ParseWmaFiles.py
+++ ParseWmaFiles.py
@@ -63,7 +63,6 @@ pipeline_artist_albums_2 = [
     {
         "$group": {
             "_id": "$_id.artist",
-            # "track_count": { "$push": "$track_count" },
             "album_count": { "$sum": 1},
             "albums": {
                 "$push": {
@@ -82,8 +81,6 @@ def artist_to_album_mapper(track_json):
 
 
 def probe_file(filename):
-    # os.chdir(os.path.expanduser("~/ffmpeg"))
-    # os.chdir('ffmpeg')
     ffprobe_path = './ffprobe'
     # ffprobe_path = os.path.expanduser("~/ffmpeg") + '/ffprobe
     cmnd = [ffprobe_path, '-v', 'quiet', '-print_format', 'json', '-show_format', '-hide_banner', filename]
@@ -93,32 +90,17 @@ def probe_file(filename):
     record_filename = p['format']['filename']
     logger.debug('record-filename: ' + record_filename)
     logger.debug(p)
-    # {'format': {'filename': "/Volumes/DATA/ServerFolders/Music/Abbey Lincoln/A Turtle's Dream/01_Throw It Away.wma"
     query = {'format.filename': record_filename}
-    logger.debug('mongo findOne: ')
-    # logger.debug(music_records.find_one(query))
 
     result = music_records.replace_one(query, p, True)
     logger.debug('One post: {0}'.format(result))
-
-    # logger.debug('p: {0}'.format(p))
-
     # pprint(p) # ['format']['tags'])
-    # pprint(len(p.keys()))
 
     db.music_records.aggregate(pipeline_format)
     db.music_records.aggregate(pipeline_artist)
     db.music_records.aggregate(pipeline_album)
     db.music_records.aggregate(pipeline_artist_albums)
     db.artist_album_tracks_raw.aggregate(pipeline_artist_albums_2)
-    # nothing to group until its in the db
-    # for key, group in itertools.groupby(p, lambda x: x['format']['tags']['album_artist']):
-    #     for thing in group:
-    #         print("A %s is a %s.".format(thing, key))
-    #
-    # album_groups = itertools.groupby(p, artist_to_album_mapper)
-    # db.albums.insert(album_groups)
-
 
 logger.info('Starting the Run')
 for root, dirs, files in os.walk(music_path):
@@ -128,74 +110,5 @@ for root, dirs, files in os.walk(music_path):
         logger.debug(len(path) * '--- ' + file + ' ' + os.path.join(root, file))
         if file.endswith(".wma") or file.endswith(".mp3") or file.endswith(".flac"):
             probe_file(os.path.join(root, file))
-
-mapper = Code("""
-function() {
-    emit(this.format.tags.album_artist, {title: this.format.tags.title, album: this.format.tags.album,  album_artist: this.format.tags.album_artist, all: this});
-}
-""")
-
-reducer = Code("""function(key,values) {
-    var result = {};
-    var commentFields = {
-        "album": '',
-        "title": '', 
-        "all": ''
-    };
-    // "album_artist": '',
-    // 
-
-    values.forEach(function(value) {
-        var field;
-        var albumValue;
-        if ("title" in value) {
-            if (!("albums" in result)) {
-                result.albums = [];
-            }
-            result.albums.forEach(function (a) {
-                if (a.album == value.album) {
-                    if (!("albumObject" in a)) {
-                        a.albumObject = {};
-                    }
-                    if (!("songs" in a.albumObject)) {
-                        a.albumObject.songs = [];
-                    }
-                    a.albumObject.songs.push(value);
-                }
-            });
-            result.albums.push(value);
-        } else if ("albums" in value) {
-            if (!("albums" in result)) {
-                result.albums = [];
-            }
-            result.albums.push.apply(result.albums, value.albums);
-        }
-        for (field in value) {
-            if (value.hasOwnProperty(field) && !(field in commentFields)) {
-                result[field] = value[field];
-            }
-        }
-    });
-    return result;
-    }
-""")
-
-# values.forEach(function(value) {
-#         var field;
-#         if ("comment" in value) {
-#             if (!("comments" in result)) {
-#                 result.comments = [];
-#             }
-#     var total = 0;
-#     for (var i = 0; i < values.length; i++) {
-#         return values;
-#         total += values[i];
-#     }
-#     return total;
-# }
-
-# results = music_records.map_reduce(mapper, reducer, out="titles")
-# for doc in results.find():
-#     logger.info(doc)
 
 logger.info('Completed Run')
